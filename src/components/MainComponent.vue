471290af8a3faaf4e3d0a8dc36f6296c98bce49a
Anonius131 <antomendetta@gmail.com>
2023-01-18 19:15:22 +0100

    creating and styling MainComponent

---
+++ src/components/MainComponent.vue
@@ -0,0 +1,22 @@
+<template>
+   <div id="hero-container" class="col-12 col-md-6 py-5 px-5">
+      <h1>Hello</h1>
+   </div>
+</template>
+
+<script>
+   export default {
+      
+   }
+</script>
+
+<style lang="scss">
+   
+   #hero-container {
+      background-color: #f2f2f2;
+
+      h1 {
+         font-size: 3rem;
+      }
+   }
+</style>
