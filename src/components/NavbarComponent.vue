e6f3790ab77350dc58d8ccdd708ba0818327a423
Anonius131 <antomendetta@gmail.com>
2023-01-19 08:39:48 +0100

    creating navbar items

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -1,5 +1,17 @@
 <template>
-   
+   <nav class="navbar">
+      <ul class="navbar-nav">
+         <li class="nav-item">
+            <a href="#">Here</a> you can see <a href="#">my works</a>
+         </li>
+         <li class="nav-item">
+            Something <a href="#">about me</a>
+         </li>
+         <li class="nav-item">
+            My <a href="#">skills</a>
+         </li>
+      </ul>
+   </nav>
 </template>
 
 <script lang="ts">
@@ -9,5 +21,5 @@
 </script>
 
 <script lang="scss">
-
+   
 </script>
