3fa0f4deda619f75aa5310d6b368d714bd82d6b8
Anonius131 <antomendetta@gmail.com>
2023-01-20 10:26:10 +0100

    build component template

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,5 +1,22 @@
 <template>
-   
+   <section id="about-section" class="py-5 px-5">
+      <div class="col-12">
+         <h4>
+            <router-link to="/">Back</router-link>
+         </h4>
+      </div>
+      <div class="row mb-3">
+         <div class="col-12">
+            <div class="image-box"></div>
+         </div>
+      </div>
+      <div class="row">
+         <div class="col-12">
+            <h1>About me</h1>
+            <p>I’m an Italian based Web Developer and UX/UI Designer. Passionate about creative code, minimalism and bold color combinations.</p>
+         </div>
+      </div>
+   </section>
 </template>
 
 <script>
@@ -8,6 +25,14 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style scoped lang="scss">
+   #about-section {
+      height: 100%;
 
+      .image-box {
+
+         background-color: #f2f2f2;
+         height: 400px
+      }
+   }
 </style>
