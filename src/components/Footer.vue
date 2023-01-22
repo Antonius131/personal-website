8c0a02703e6313ad5430954eff7a2e4e981fcb9a
Anonius131 <antomendetta@gmail.com>
2023-01-22 10:23:25 +0100

    creating new component

---
+++ src/components/Footer.vue
@@ -0,0 +1,44 @@
+<template>
+   <footer>
+      <ul class="list-unstyled d-flex">
+         <li v-for="(item, index) in items" :key="index">
+            <p class="mb-0" :class="item.style">
+               {{ item.name }}
+            </p>
+         </li>
+      </ul>
+   </footer>
+</template>
+
+<script>
+export default {
+   data() {
+      return {
+         items: [
+            {
+               name: 'Contact',
+               style: 'text-start'
+            },
+            {
+               name: 'LinkedIn',
+               style: 'text-center'
+            },
+            {
+               name: 'GitHub',
+               style: 'text-end'
+            }
+         ]
+      }
+   }
+}
+</script>
+
+<style scoped lang="scss">
+   ul {
+      text-align: justify;
+
+      li {
+         width: calc(100% / 3);
+      }
+   }
+</style>
