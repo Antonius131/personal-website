176c0b5555ae9d38be44994d7f5700f49ec6b4ec
Anonius131 <antomendetta@gmail.com>
2023-01-22 09:35:28 +0100

    fixing component import and styling

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -4,32 +4,26 @@
          <h1>Hello</h1>
          <p>I’m an Italian based Web Developer and UX/UI Designer. Passionate about creative code, minimalism and bold color combinations. <i>SelfImproving</i> founder and <i>FeedingCats</i> contributor. :)</p>
       </div>
-      <NavbarComponent />
+      <NavBar />
    </div>
 </template>
 
 <script>
-   import NavbarComponent from './NavbarComponent.vue';
+   import NavBar from './NavBar.vue';
 
    export default {
       components: { 
-      NavbarComponent
+         NavBar
       }
    }
 </script>
 
 <style lang="scss">
-   
    #hero-container {
       background-color: #f2f2f2;
 
       h1 {
          font-size: 3rem;
-      }
-
-      p {
-         font-size: 1.25rem;
-         font-weight: 200;
       }
    }
 </style>
