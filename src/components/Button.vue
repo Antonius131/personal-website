8a3b44a4cfd484139a0303a870249ea6d0f745db
Anonius131 <antomendetta@gmail.com>
2023-01-22 09:34:28 +0100

    creating new components

---
+++ src/components/Button.vue
@@ -0,0 +1,17 @@
+<template>
+  <button type="button" class="btn btn-dark" @click="$emit('click', method)">
+   <slot />
+  </button>
+</template>
+
+<script>
+export default {
+   props: {
+      method: Function
+   }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/GoBack.vue
@@ -0,0 +1,15 @@
+<template>
+   <div class="row mb-2">
+      <div class="col-12">
+         <h4>
+            <router-link to="/">Back</router-link>
+         </h4>
+      </div>
+   </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss">
+</style>
