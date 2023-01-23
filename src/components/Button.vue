dce0e61c8dde38b5f4f2707801404d4c002905db
Anonius131 <antomendetta@gmail.com>
2023-01-23 08:18:47 +0100

    cleaning script tag

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,14 +1,11 @@
 <template>
-  <button type="button" class="btn btn-dark" @click="$emit('click', method)">
-   <slot />
-  </button>
+   <button type="button" class="btn btn-dark" @click="$emit('click', method)">
+      <slot />
+   </button>
 </template>
 
 <script>
 export default {
-   props: {
-      method: Function
-   }
 }
 </script>
 
