75465f6800202e1dd39a9784dab9b5a0df185f3e
Anonius131 <antomendetta@gmail.com>
2023-01-24 17:44:21 +0100

    create component to redirect users based on path

---
+++ src/components/Init.vue
@@ -0,0 +1,23 @@
+<template>
+</template>
+
+<script>
+export default {
+   name: 'Init',
+   methods: {
+      isMobile() {
+         if (screen.width <= 768) {
+            this.$router.push({ path:'/mobile/' });
+         } else{
+            this.$router.push({ path: '/desktop/' });
+         }
+      }
+   },
+   created() {
+      this.isMobile()
+   }
+}
+</script>
+
+<style>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,28 +3,54 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-import Main from '../components/Main'
-import Portfolio from '../components/Portfolio'
-import AboutMe from '../components/AboutMe'
-import MySkills from '../components/MySkills'
+import Init from '../components/Init'
+
+/**
+ *  Mobile components
+ */
+import Main from '../components/mobile/Main'
+import Portfolio from '../components/mobile/Portfolio'
+import AboutMe from '../components/mobile/AboutMe'
+import MySkills from '../components/mobile/MySkills'
+
+/**
+ * Desktop components
+ */
+import MainDesktop from '../components/desktop/MainDesktop'
 
 const routes = [
   {
     path: '/',
+    component: Init
+  },
+
+  /**
+   * Mobile routes
+   */
+  {
+    path: '/mobile',
     component: Main
   },
   {
-    path: '/works',
+    path: '/mobile/works',
     component: Portfolio
   },
   {
-    path: '/about',
+    path: '/mobile/about',
     component: AboutMe
   },
   {
-    path: '/skills',
+    path: '/mobile/skills',
     component: MySkills
-  }
+  },
+
+  /**
+   * Dsektop routes
+   */
+  {
+    path: '/desktop',
+    component: MainDesktop
+  },
 ]
 
 const router = new VueRouter({
