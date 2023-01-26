b24dc069009a3bda4ebcefc793c9ae376f6ed702
Anonius131 <antomendetta@gmail.com>
2023-01-26 12:22:21 +0100

    change Root template structure

--- src/Root.vue
+++ src/Root.vue
@@ -1,7 +1,14 @@
 <template>
    <div id="root" class="container-fluid d-none d-md-block">
      <div class="row" id="main-wrapper">
-       <MainDesktop />
+         <div class="col-6">
+            <MainDesktop />
+         </div>
+         <div id="content" class="col-6">
+            <transition name="fade" mode="out-in">
+               <router-view />
+            </transition>
+         </div>
      </div>
    </div>
  </template>
@@ -17,19 +24,35 @@
    }
  </script>
  
- <style lang="scss">
- body {
+<style scoper lang="scss">
+   body {
    margin: 0;
    padding: 0;
    box-sizing: border-box;
-     
-     #root {
-       font-family: "Work Sans", sans-serif;
-       color: #2c3e50;
-       
-       #main-wrapper {
+      
+      #root {
+         font-family: "Work Sans", sans-serif;
+         color: #2c3e50;
+         
+         #main-wrapper {
          height: 100vh;
-       }
-     }
- }
+         overflow: auto;
+         }
+
+         #content {
+         padding: 7rem 12rem;
+         height: 100vh;
+         overflow: auto;
+      }
+      }
+   }
+
+   .fade-enter, .fade-leave-to {
+   opacity: 0;
+   transform: translateX(50px);
+   }
+
+   .fade-enter-active, .fade-leave-active {
+   transition: all 0.2s ease;
+   }
  </style> 
